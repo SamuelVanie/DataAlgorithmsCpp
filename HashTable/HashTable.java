9f00a0684633d340c28826416fe12dd85b391bd8
SamuelVanie <damsektry@gmail.com>
2025-02-18 18:04:38 -0500

    ci: untrack garbages

---
+++ HashTable/HashTable.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HashTable {
+
+    public HashTable() {
+    }
+
+
+    public static class HashTableTest {
+
+        @Test
+        public void testPutAndGet() {
+            
+        }
+
+    }
+}

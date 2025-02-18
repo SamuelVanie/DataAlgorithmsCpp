8a7011152a46ffafad0d44b8ce4411c6890466d7
SamuelVanie <damsektry@gmail.com>
2025-02-18 17:32:07 -0500

    fix: junit setup to the flake for direct compilationc

--- flake.nix
+++ flake.nix
@@ -4,12 +4,20 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (system: let
       pkgs = import nixpkgs { inherit system; };
+      junitJar = pkgs.fetchurl {
+        url = "https://repo1.maven.org/maven2/junit/junit/4.13.2/junit-4.13.2.jar";
+        sha256 = "sha256-jklbY0Rp1k+4rPo0laBly6zIoP/1XOHjEAe+TBbcV9M=";
+      };
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           jdt-language-server
           jdk
         ];
+        shellHook = ''
+          export CLASSPATH="${junitJar}:$CLASSPATH"
+        '';
       };
     });
 }
+
